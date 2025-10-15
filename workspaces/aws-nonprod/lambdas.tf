9e743ed2915590e6c18d0f7cb974f8e7a1b6f301
victorseara <victorseara.me@gmail.com>
2025-10-15 16:55:33 -0300

    refactor: create dedicated lambda file

--- workspaces/aws-nonprod/lambdas.tf
+++ workspaces/aws-nonprod/lambdas.tf
@@ -1,28 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1" # TODO: Replace with your desired AWS region
-}
-
 # Before applying this configuration, ensure you have a zip file
 # containing your lambda function code at the path specified in `source_path`.
 # For example, you can create a zip from the `src` directory:
 # zip -j fastify-lambda.zip src/index.js
 
 module "fastify_lambda_bff" {
-  source = "../../../modules/bff"
+  source = "../modules/bff"
 
   function_name = "fastify-lambda"
   handler       = "index.handler"
   runtime       = "nodejs22.x"
-  source_path   = "../../../lambda-placeholder/placeholder.zip"
+  source_path   = "../lambda-placeholder/placeholder.zip"
 
   # When sourcing from S3, provide the bucket and key.
   # The CI/CD pipeline of your application should upload the zip artifact to this location.
